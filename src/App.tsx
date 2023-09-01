552f27cdea7f88def39b4492ed95408ea8422de2
Guilherme Fragnani <guilhermefragnani@icloud.com>
2023-09-01 19:47:22 -0300

    fisrt commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import LoginScreen from './components/LoginScreen';
+import axios from 'axios';
 
 function App() {
-    const [count, setCount] = useState(0);
 
-    return (<>
-    <LoginScreen/>
-    </>);
+    const [authToken, setAuthToken] = useState(
+        localStorage.getItem('@token')
+    );
+
+    axios.defaults.headers.common['Authorization'] = authToken;
+
+    if (!authToken?.length) {
+        return (
+                <LoginScreen setAuthToken={setAuthToken} />
+        );
+    }
+
 }
 
 export default App;
