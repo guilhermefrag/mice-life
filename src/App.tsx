be43f6f8eadd7e49da1ad7350ffff966b82f3fd8
Guilherme Fragnani <guilhermefragnani@icloud.com>
2023-09-05 23:11:41 -0300

    add grafico

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import LoginScreen from './components/LoginScreen';
+import GraphicExample from './components/GraphicExample';
 import axios from 'axios';
 
 function App() {
@@ -21,7 +22,7 @@ function App() {
 
     return(
         <>
-            <h1>Home</h1>
+            <GraphicExample />
         </>
     )
 
