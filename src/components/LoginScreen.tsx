12170a8802ad51d23231dbc4d9b759b8992263d9
Guilherme Fragnani <guilhermefragnani@icloud.com>
2023-09-07 11:48:20 -0300

    arrumando o css do vitor

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -2,6 +2,9 @@ import { TextField, Button } from '@mui/material';
 import axios from 'axios';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import '../css/LoginScreen.css'
+
+import Rato from '../img/rato2.jpg'
 
 type UserLogin = {
     username: string;
@@ -33,25 +36,33 @@ export default function LoginScreen({ setAuthToken }: LoginScreenProps) {
     };
 
     return (
-        <div>
-            <form action="">
+        <div className='container-form'>
+            <form action="" className="formulario-login">
+                <img 
+                src={Rato} 
+                className='img-logo'
+                />
                 <TextField
                     label="Username"
                     variant="outlined"
+                    className='input-text'
                     {...register('username')}
                 />
                 <TextField
                     label="Password"
                     variant="outlined"
+                    type='password'
+                    className='input-text'
                     {...register('password')}
                 />
                 <Button
                     variant="contained"
                     type="submit"
+                    style={{background:'#b9b9b9f7'}}
                     size="large"
                     onClick={handleSubmit((user) => handleLogin(user))}
                 >
-                    Contained
+                    Login
                 </Button>
             </form>
         </div>
