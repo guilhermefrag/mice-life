968584e0f8fb11c8d0f8671b4cc23bfc9bf25e10
Guilherme Fragnani Bez Fontana <guilhermefragnani@icloud.com>
2023-09-14 13:12:49 -0300

    graphic

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Cage from './pages/Cage';
+import GraphicExample from './components/GraphicExample';
 
 export default function Router() {
     return (
         <Routes>
-            <Route path="/" element={<Cage />} />
+            <Route path="/" element={<GraphicExample />} />
         </Routes>
     );
 }
